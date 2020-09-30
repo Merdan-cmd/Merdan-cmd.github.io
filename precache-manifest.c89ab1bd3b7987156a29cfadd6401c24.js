b29c3c25a7ef9cebec79b886d2283c09e29af755
Merdan-cmd <merdan.oezdogan@gmail.com>
2020-09-30 16:49:01 +0200

    Deploy

---
+++ precache-manifest.c89ab1bd3b7987156a29cfadd6401c24.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7003e386b139edf93748",
+    "url": "/css/app.1f539f94.css"
+  },
+  {
+    "revision": "2b21ce130eb100472af792cb1bd67bee",
+    "url": "/img/Football.2b21ce13.png"
+  },
+  {
+    "revision": "67697705da47bf7f3f370498f2c6044d",
+    "url": "/img/TRYMEBITCH.67697705.png"
+  },
+  {
+    "revision": "335dd3420054fee0ab8d5aff0920598a",
+    "url": "/img/bottle.335dd342.jpg"
+  },
+  {
+    "revision": "a4715717dd7d201db9add22f74db0a35",
+    "url": "/img/marshmallow.a4715717.png"
+  },
+  {
+    "revision": "9e69819c64259283fc28bcdf3c438f0a",
+    "url": "/img/ping.9e69819c.jpg"
+  },
+  {
+    "revision": "49590f276f1928373852283bc73cff7d",
+    "url": "/img/pong.49590f27.jpg"
+  },
+  {
+    "revision": "7b039eeb4bee091e418da66b207d606c",
+    "url": "/img/pullup.7b039eeb.jpg"
+  },
+  {
+    "revision": "456aaffcf09e4481fd1e90de56724f47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7003e386b139edf93748",
+    "url": "/js/app.a3a462a6.js"
+  },
+  {
+    "revision": "7691aa4a37dc7cf1f0eb",
+    "url": "/js/chunk-vendors.6ed33d3e.js"
+  },
+  {
+    "revision": "ed1d6553ba648669165d2b5acfc7375a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
